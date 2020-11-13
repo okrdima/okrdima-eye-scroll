09f032874ebd6b82569df81cf76e63b42c513b75
okrdima <okrdima@gmail.com>
2020-11-13 13:14:42 +0200

    fix server fix process.env.PORT in app.listen

--- server.js
+++ server.js
@@ -1,30 +1,8 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
 
-app.use(cors())
 app.use(express.static(__dirname))
 
-const port = process.env.PORT || 3000
-
-app.get('/',function (request, response) {
-    response.sendFile(__dirname + "/index.html");
-
-});
-
-app.listen(3000);
-// const express = require('express')
-// var app = express()
-//
-// const port = 4000
-//
-// app.use(express.static(__dirname))
-//
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// })
-//
-//
-// app.listen(port, () => {
-//     console.log(`Example app listening at http://localhost:${port}`)
-// })
+app.get('/',function (request, response) {response.sendFile(__dirname + "/index.html")
+})
+app.listen(process.env.PORT);
